30e0fd0e1c4e43d0fca8dbe592f3db45b149f4f8
mptmadhushan <tharindu.madhushan@axiatadigitallabs.com>
2022-10-25 11:56:23 +0530

    first commit

---
+++ app/models/index.js
@@ -0,0 +1,46 @@
+const dbConfig = require("../config/db.config");
+
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+  host: dbConfig.HOST,
+  dialect: dbConfig.dialect,
+  // operatorsAliases: false,
+
+  pool: {
+    max: dbConfig.pool.max,
+    min: dbConfig.pool.min,
+    acquire: dbConfig.pool.acquire,
+    idle: dbConfig.pool.idle,
+  },
+});
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.user = require("./user.model.js")(sequelize, Sequelize);
+db.role = require("./role.model.js")(sequelize, Sequelize);
+db.posts = require("./posts.model")(sequelize, Sequelize);
+db.comments = require("./comments.model")(sequelize, Sequelize);
+db.ads = require("./ads.model")(sequelize, Sequelize);
+
+db.posts.hasMany(db.comments, { as: "comments" });
+db.comments.belongsTo(db.posts, {
+  foreignKey: "PostId",
+  as: "posts",
+});
+db.role.belongsToMany(db.user, {
+  through: "user_roles",
+  foreignKey: "roleId",
+  otherKey: "userId",
+});
+db.user.belongsToMany(db.role, {
+  through: "user_roles",
+  foreignKey: "userId",
+  otherKey: "roleId",
+});
+
+db.ROLES = ["user", "admin", "provider"];
+
+module.exports = db;
