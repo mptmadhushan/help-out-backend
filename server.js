b0c776eb4313f5338304720ecd8a3a75f8f646e9
mptmadhushan <tharindu.madhushan@axiatadigitallabs.com>
2022-10-25 11:59:04 +0530

    report ui

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ require("./app/routes/ads.routes")(app);
 require("./app/routes/contact.cpg.mail.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 3000;
+const PORT = 3002;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
